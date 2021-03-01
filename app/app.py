c953d79a5eaa649ed3e9de964229421c9c5e7916
ryuneeee <ryuneeee@gmail.com>
2021-03-02 00:45:43 +0900

    add holdings

--- app/app.py
+++ app/app.py
@@ -15,5 +15,11 @@ def price(ticker):
     return stock.price
 
 
+@app.route('/holdings/<ticker>')
+def holders(ticker):
+    stock = Ticker(ticker)
+    return stock.fund_holding_info
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
